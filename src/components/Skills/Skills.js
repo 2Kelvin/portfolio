b02bb41abe51c078f6a0f36c9aeb8dc83cb7961b
2Kelvin <kvin.mwangi254@gmail.com>
2022-06-30 22:51:30 +0300

    added & spaced technologies section

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,7 +1,6 @@
 import "./Skills.css";
 import { BsFolder2Open } from "react-icons/bs";
 import { GoHome } from "react-icons/go";
-import { TbBrandReactNative } from "react-icons/tb";
 import { SiHtml5, SiCss3, SiJavascript, SiReact, SiGithub } from "react-icons/si";
 import { IoIosContact } from "react-icons/io";
 import { IoCallOutline, IoStarHalfOutline } from "react-icons/io5";
@@ -22,35 +21,32 @@ const Skills = () => {
                 <div id="skillsId" className="skillsContent">
                     <div className="skillsContentTitle">
                         <h2 className="kelvin">Skills</h2>
-                    </div>
-                    
-                    <div>
                         <h1>What I can Do</h1>
                     </div>
-                    
+                   
                     <div className="skillsContentTechnologies">
                         <div className="technologyTile">
-                            <SiHtml5 style={{color:"orangered"}}/>
+                            <SiHtml5 style={{color:"orangered", alignSelf:"center"}}/>
                             <p>HTML</p>
                         </div>
                         <div className="technologyTile">
-                            <SiCss3 style={{color:"dodgerblue"}}/>
+                            <SiCss3 style={{color:"dodgerblue", alignSelf:"center"}}/>
                             <p>CSS</p>
                         </div>
                         <div className="technologyTile">
-                            <SiJavascript style={{color:"yellow"}}/>
+                            <SiJavascript style={{color:"yellow", alignSelf:"center"}}/>
                             <p>Javascript</p>
                         </div>
                         <div className="technologyTile">
-                            <SiReact style={{color:"#61DBFB"}}/>
+                            <SiReact style={{color:"#61DBFB", alignSelf:"center"}}/>
                             <p>React</p>
                         </div>
                         <div className="technologyTile">
-                            <SiGithub style={{color:"black"}}/>
+                            <SiGithub style={{color:"black", alignSelf:"center"}}/>
                             <p>Github</p>
                         </div>
                         <div className="technologyTile">
-                            <TbBrandReactNative style={{color:"#61DBFB"}}/>
+                            <SiReact style={{color:"#61DBFB", alignSelf:"center"}}/>
                             <p>React Native</p>
                         </div>
                    </div>
